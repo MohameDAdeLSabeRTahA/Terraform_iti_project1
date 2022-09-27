19e76a9753f38a861fee5ff1df9959db5a2d3af5
Mohamed Adel <mohamed.adel.saber.taha@gmail.com>
2022-09-27 13:21:22 +0200

    Testing the change tigger

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
-# module "infra_module" {
-#   source              = "./Infra_module"
-#   vpc_cidr            = var.vpc_cidr
-#   AZ1                 = var.AZ1
-#   AZ2                 = var.AZ2
-#   PublicSubnet1_CIDR  = var.PublicSubnet1_CIDR
-#   PublicSubnet2_CIDR  = var.PublicSubnet2_CIDR
-#   PrivateSubnet1_CIDR = var.PrivateSubnet1_CIDR
-#   PrivateSubnet2_CIDR = var.PrivateSubnet2_CIDR
-# }
+module "infra_module" {
+  source              = "./Infra_module"
+  vpc_cidr            = var.vpc_cidr
+  AZ1                 = var.AZ1
+  AZ2                 = var.AZ2
+  PublicSubnet1_CIDR  = var.PublicSubnet1_CIDR
+  PublicSubnet2_CIDR  = var.PublicSubnet2_CIDR
+  PrivateSubnet1_CIDR = var.PrivateSubnet1_CIDR
+  PrivateSubnet2_CIDR = var.PrivateSubnet2_CIDR
+}
 
 module "serverless_module" {
   source = "./serverless_module"
