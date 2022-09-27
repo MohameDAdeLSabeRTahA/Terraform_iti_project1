7d20b164e4862624d91fc6b41a28a2bd1559cefb
Mohamed Adel <mohamed.adel.saber.taha@gmail.com>
2022-09-27 13:19:14 +0200

    Adding Serverless module that contains the Lambda and ses services

---
+++ serverless_module/lambda.tf
@@ -0,0 +1,34 @@
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
+
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "sts:AssumeRole"
+            ],
+            "Principal": {
+                "Service": [
+                    "lambda.amazonaws.com"
+                ]
+            }
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_lambda_function" "emil_trigger_lambda" {
+  filename      = "./serverless_module/compressed_code/python_script.zip"
+  function_name = "email_trigger"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "python_script.lambda_handler"
+  runtime       = "python3.9"
+
+  depends_on = [
+    aws_ses_email_identity.ses_email
+  ]
+}
