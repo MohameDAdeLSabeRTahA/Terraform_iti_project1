85abf6f5b089127e521c75b4af5683fb03dff45a
Mohamed Adel <mohamed.adel.saber.taha@gmail.com>
2022-09-27 09:34:04 +0200

    Editing map_public_ip_on_launch for private subnets

--- infrastructure.tf
+++ infrastructure.tf
@@ -24,13 +24,13 @@ resource "aws_subnet" "PrivateSubnet1" {
   vpc_id     = aws_vpc.terraform_vpc.id
   availability_zone = var.AZ1
   cidr_block = var.PrivateSubnet1_CIDR
-  map_public_ip_on_launch = "true"
+  map_public_ip_on_launch = "false"
 }
 resource "aws_subnet" "PrivateSubnet2" {
   vpc_id     = aws_vpc.terraform_vpc.id
   availability_zone = var.AZ2
   cidr_block = var.PrivateSubnet2_CIDR
-  map_public_ip_on_launch = "true"
+  map_public_ip_on_launch = "false"
 }
 
 ################################ EIP & NAT Gateway ################################
