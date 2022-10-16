c180cc5f0b77013777aa843e7334059c4c7418e0
Mohamed Adel <mohamed.adel.saber.taha@gmail.com>
2022-10-16 22:59:50 +0200

    Modify outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,17 @@
 output "vpc_id" {
-  value = module.infra_module.aws_vpc.terraform_vpc.id
+  value = module.infra_module.vpc_id
 }
 
 output "PublicSubnet1_id" {
-  value = module.infra_module.aws_subnet.PublicSubnet1.id
+  value = module.infra_module.PublicSubnet1_id
 }
 output "PublicSubnet2_id" {
-  value = module.infra_module.aws_subnet.PublicSubnet2.id
+  value = module.infra_module.PublicSubnet2_id
 }
 
 output "PrivateSubnet1_id" {
-  value = module.infra_module.aws_subnet.PrivateSubnet1.id
+  value = module.infra_module.PrivateSubnet1_id
 }
 output "PrivateSubnet2_id" {
-  value = module.infra_module.aws_subnet.PrivateSubnet2.id
+  value = module.infra_module.PrivateSubnet2_id
 }
